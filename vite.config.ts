51c4d3d3b384b91d806b1f562a027370d9a40b46
MauricioBarrientos <mauricio.barrientos.f@gmail.com>
2026-01-21 14:34:31 -0300

    Add GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dashboard_analitico/',
   css: {
     postcss: './postcss.config.cjs', // This will help with Tailwind processing
   },
